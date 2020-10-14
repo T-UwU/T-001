5f9fd3b48e2ef65d7b797224fc8b09f048f5d50a
T-UwU <israjr100164@gmail.com>
2020-10-13 19:37:36 -0500

    Subir kata nwn

---
+++ Kata_War.py
@@ -0,0 +1,9 @@
+#def areYouPlayingBanjo(name):
+#    name_0 = name.find("r",0,2)
+#    name_1 = name.find("R",0,2)
+#    if name_0 == 0 or name_1 == 0:
+#        name = (name + " plays banjo")
+#    else:
+#        name = (name + " does not play banjo")
+#    return name
+
