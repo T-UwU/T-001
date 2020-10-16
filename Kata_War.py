e3334b767c9e218f5faeb0bd07893bd493d01193
T-UwU <israjr100164@gmail.com>
2020-10-16 17:21:48 -0500

    Mas kata

--- Kata_War.py
+++ Kata_War.py
@@ -11,28 +11,12 @@
 #    i = i*2
 #double_integer(2)
 
-def fix(paragraph):
-    lst = []
-    lst_2 = []
-    p = paragraph.split(".")
-    separator = '.'
-    
-    for i in p:
-        g = i.split(" ")
-        lst_2.append(g)
-        for x in lst_2:
-            for x_1 in x:
-                if x.index(x_1) == 1 or x.index(x_1) == 0 or  x.index(x_1) == 2:
-                    r = x_1.title()
-                    z_1 = r + " "
-                    lst.append(z_1)
-                else:
-                    z_2 = x_1 + " "
-                    lst.append(z_2)
-                
-    tup = lst_2[0] + lst_2[1]
-    z = separator.join(tup)
-    
-    print(z)
-    
-fix("hello. my name is inigo montoya. you killed my father. prepare to die.")
+#def fix(paragraph):
+#    print( '. '.join(s.capitalize() for s in paragraph.split('. ')))
+#
+#fix("hello. my name is inigo montoya. you killed my father. prepare to die.")
+
+#def past(h, m, s):
+#    mil = 0 + s*1000 + m*60000 + h*3600000
+#    return(mil)
+#past(0,0,1)
